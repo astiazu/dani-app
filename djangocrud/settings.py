efcdc0fe58de3acad5044783294d88f186c7bbd9
astiazu <astiazu@gmail.com>
2023-09-12 13:10:46 -0300

    volvi a modificar el setting

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -83,13 +83,13 @@ WSGI_APPLICATION = 'djangocrud.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
         
-        #'default': dj_database_url.config(
+        'default': dj_database_url.config(
         # Feel free to alter this value to suit your needs.
-        #default='postgresql://postgres:postgres@localhost:5432/mysite',
-        #conn_max_age=600)
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        conn_max_age=600)
         
     }
 }
